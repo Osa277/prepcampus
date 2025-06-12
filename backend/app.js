b251e9e2b5115796a0d82e3ef00e5e9f80f7ab29
anjorin001 <anjorinfavour123@gmail.com>
2025-06-12 18:44:14 +0100

    backend folders setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors({origin: "http://localhost/3000"}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const MongoConnect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("mongoose connected")
+    } catch (err) {
+        console.error("err connecting to mongoodb: ", err)
+        process.exit(1)
+    }
+}
+MongoConnect()
+
+
+app.listen(PORT)
